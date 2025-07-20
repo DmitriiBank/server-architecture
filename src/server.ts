76cc4663b42a0e7f4fe639e8dfe94bb9fe54e861
DmitriiBank <dm.skakov@gmail.com>
2025-07-20 18:15:55 +0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import {createServer} from "node:http";
+import {PORT} from "./config/userServerConfig.ts";
+import {userRouters} from "./routers/userRouters.ts";
+import {UserController} from "./controllers/UserController.ts";
+import {UserServiceEmbeddedImpl} from "./service/UserServiceEmbeddedImpl.ts";
 
 export const launchServer = () => {
+    const userService = new UserServiceEmbeddedImpl();
     const userController:UserController = new UserController(userService)
 
     createServer(async (req, res) => {
         await userRouters(req, res, userController)
-    }).listen(3005, () => {
-        console.log('UserServer runs at htpp://localhost:3005')
+    }).listen(PORT, () => {
+        console.log(`UserServer runs at http://localhost:${PORT}`)
     })
 }
